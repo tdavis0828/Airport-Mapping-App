976418e5d85203c942db5db87549a81d17495cb8
tdavis0828 <davis.t828@gmail.com>
2022-05-03 13:36:24 -0700

    code for fitlering method

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,19 +6,64 @@ const List = () => {
     const usAirports = airports.filter((airport) =>
       airport.country.includes("United States")
     );
-    let codes = [];
-    for (const airports of usAirports) {
-      codes.push(airports.code);
+    const filteredAirports = usAirports.filter(
+      (airport) => airport.runway_length > 12000
+    );
+    let lon = [];
+    let lat = [];
+    for (const airports of filteredAirports) {
+      lon.push(airports.lon);
+      lat.push(airports.lat);
     }
-    return codes;
+    console.log(lon, lat);
+    // return codes;
   };
 
   const getData = async () => {
     const res = await fetch(
       `https://api.aviationapi.com/v1/airports?apt=${getCodes()}`
     );
-    const data = await res.json();
-    console.log(data);
+    const airportInfo = await res.json();
+
+    let airportData = [];
+    console.log(airportInfo);
+    for (const key in airportInfo) {
+      if (airportInfo[key].length) {
+        const {
+          city,
+          facility_name,
+          county,
+          state_full,
+          manager,
+          latitude,
+          longitude,
+          status,
+        } = airportInfo[key][0];
+
+        airportData.push({
+          facility_name,
+          city,
+          county,
+          state_full,
+          manager,
+          latitude,
+          longitude,
+          status,
+        });
+
+        console.log(
+          `Airport Name: ${facility_name}`,
+          `City: ${city}`,
+          `State: ${state_full}`,
+          `County: ${county}`,
+          `Current Manager: ${manager}`,
+          `Latitude: ${latitude}`,
+          `Longitude: ${longitude}`,
+          `Current Status: ${status}`
+        );
+      }
+    }
+    console.log(airportData);
   };
   getData();
 
